4cfb6d0b0a023a7e553051280097498c3698b2fa
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-17 11:01:38 -0700

    Replaced images with medium size images. Added an image to 404 Page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <img src="/images/utensils.jpg" alt="Utensils" />
+                <div>
+                    Photo by <a href="https://unsplash.com/@hudsoncrafted?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Debby Hudson</a> on <a href="https://unsplash.com/photos/gray-fork-spoon-and-butter-knife-on-plate-with-black-table-napkin-pomybSOZsts?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
