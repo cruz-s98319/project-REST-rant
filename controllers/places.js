c764b56db1d041db2f77b096a305687d6031e0ac
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-20 14:45:51 -0700

    added index on line 5 in index.jsx to fix a bug that was causing the restaurant links to the error 404 page

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,10 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/', (req, res) => {
+    res.render('places/index', {places})
+})
+
 router.post('/', (req, res) => {
     if (!req.body.pic) {
         //Default image if one is not provided
@@ -19,12 +23,8 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  
-router.get('/', (req, res) => {
-    res.render('places/index', {places})
-})
 
-router.get(':/id', (req, res) => {
+router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
         res.render('error404')
@@ -33,8 +33,22 @@ router.get(':/id', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
+        res.render('places/show', {place:places[id], id })
     }
 })
 
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+  
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,33 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
+                <img src={data.place.pic} alt={data.place.name} />
             </main>
+            <body>
+                <div>
+                    <h2>Rating</h2>
+                    <p>Not Rated</p>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
+                <div>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        Edit
+                    </a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </body>
         </Def>
     )
 }
