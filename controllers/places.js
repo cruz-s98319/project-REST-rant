b4622ea8c5ee1294628ca15c8eac1412e318a92a
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-21 16:49:01 -0700

    Implemented the Edit route

--- controllers/places.js
+++ controllers/places.js
@@ -33,7 +33,20 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/show', {place:places[id], id })
+        res.render('places/show', { place:places[id], id })
+    }
+})
+
+router.get('/:id/edit',(req,res)=>{
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place:places[id], id })
     }
 })
 
