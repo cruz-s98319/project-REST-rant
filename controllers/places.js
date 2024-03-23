56a423e12b81675f49b0e3fb7f7c1272a5b29d50
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-23 15:00:47 -0700

    Fixed the Show Page and Links

--- controllers/places.js
+++ controllers/places.js
@@ -125,8 +125,16 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// SHOW route
 router.get('/:id', (req, res) => {
-    res.send('GET /places/:id stub')
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/show', { place })
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
 router.put('/:id', (req, res) => {
