e6d829e88970a5b0787744c9d5b727bbea80e3aa
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-28 12:35:47 -0700

    Created and implemented a helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,32 +6,45 @@ function show (data) {
         <Def>
             <main>
                 <h1>{data.place.name}</h1>
-                <img src={data.place.pic} alt={data.place.name} />
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>
+                            Rating
+                        </h2>
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div>
+                    <h2>
+                        Comments
+                    </h2>
+                    <h3>
+                        No comments yet!
+                    </h3>
+                </div>
             </main>
-            <body>
-                <div>
-                    <h2>Description</h2>
-                    <p>Located in {data.place.city}, {data.place.state}, and serving {data.place.cuisines}</p>
-                </div>
-                <div>
-                    <h2>Rating</h2>
-                    <p>Not Rated</p>
-                </div>
-                <div>
-                    <h2>Comments</h2>
-                    <p>No comments yet!</p>
-                </div>
-                <div>
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                        Edit
-                    </a>
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">
-                            Delete
-                        </button>
-                    </form>
-                </div>
-            </body>
         </Def>
     )
 }
