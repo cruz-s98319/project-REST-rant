017a79d1dba73371696a46a0956a846d9ae39c52
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-29 13:38:42 -0700

    Added a comment form and more comment examples to other places

--- controllers/places.js
+++ controllers/places.js
@@ -1,63 +1,90 @@
 const router = require('express').Router()
 const db = require('../models')
 
-// INDEX route
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-        res.render('places/index', { places })
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error404')
-    })
+  db.Place.find()
+  .then((places)=>{
+    res.render('places/index',{places})
+  })
+  .catch(err =>{
+    console.log(err)
+    res.render('error404')
+  })
 })
 
-// POST route
 router.post('/', (req, res) => {
-    db.Place.create(req.body)
-    .then(() => {
-        res.redirect('/places')
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  if(!req.body.pic){
+    //Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+
+  db.Place.create(req.body)
+  .then(()=> {
+    res.redirect('/places')
+  })
+  .catch(err=> {
+    console.log('err',err)
+    res.render('error404')
+  })
 })
 
 router.get('/new', (req, res) => {
-    res.render('places/new')
+  res.render('places/new')
 })
 
-// SHOW route
 router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
-    .populate('comments')
-    .then(place => {
-        console.log(place.comments)
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/show',{place})
+  })
+  .catch(err=>{
+    console.log('err',err)
+    res.render('error404')
+  })
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+  res.send('PUT /places/:id stub')
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
+  res.send('DELETE /places/:id stub')
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
+  res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  console.log('post comment', req.body)
+  if (req.body.author === '') { req.body.author = undefined }
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+      .then(place => {
+          db.Comment.create(req.body)
+              .then(comment => {
+                  place.comments.push(comment.id)
+                  place.save()
+                      .then(() => {
+                          res.redirect(`/places/${req.params.id}`)
+                      })
+                      .catch(err => {
+                          res.render('error404')
+                      })
+              })
+              .catch(err => {
+                  res.render('error404')
+              })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
 })
+
+
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
