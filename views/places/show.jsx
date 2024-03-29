07f1cac49fb626d85a7a6e9bf18351de74aeaf43
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-29 13:26:55 -0700

    Populated h-thai-ml with example comment

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -40,9 +59,7 @@ function show (data) {
                     <h2>
                         Comments
                     </h2>
-                    <h3>
-                        No comments yet!
-                    </h3>
+                    {comments}
                 </div>
             </main>
         </Def>
